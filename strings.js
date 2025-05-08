f8ae895f302564445cc9282e98cf4af8b579b08e
HæLL <israelaraujo.emply@gmail.com>
2025-05-08 14:03:52 -0300

    Studing about Strings in JS

---
+++ strings.js
@@ -0,0 +1,24 @@
+/* alert("Hello, World!"); */
+
+/* var message = "Hello, World!";
+alert(message); */
+
+/* let name = "John";  */
+
+/* const message = "Hello, World!";
+
+console.log(message.length);
+ */
+
+/* const firstName = "Haell";
+const lastName = "Tec";
+
+const names = "Haell, Tec, Teste";
+const name = "Haell";
+
+console.log(firstName + " " + lastName); // Concatenation
+console.log(`Meu nome é  ${firstName.toUpperCase()} ${lastName.toLowerCase()}`); // Template string   
+
+console.log(names.split(",")); // Split
+console.log(name.split("")); // Split */
+
